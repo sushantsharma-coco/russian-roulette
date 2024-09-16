fbfbb6e26b4e480e595a73b15e8fc3f1ab66252f
sushantsharma-coco <cocosingh456@gmail.com>
2024-09-16 18:53:38 +0530

    events added to class poker

--- src/lib/classes/poker.ts
+++ src/lib/classes/poker.ts
@@ -14,10 +14,34 @@ export class Poker {
       console.log("+++++++++CONNECTION+++++++++");
 
       clientSocket.on("START_MATCH", this.onStartGame.bind(this));
+
+      clientSocket.on("CALL", this.onCall.bind(this));
+
+      clientSocket.on("CHECK", this.onCheck.bind(this));
+
+      clientSocket.on("FOLD", this.onFold.bind(this));
+
+      clientSocket.on("RAISE", this.onRaise.bind(this));
     } catch (error: any) {
       console.error("error in onConnect", error?.message);
     }
   }
 
   async onStartGame(clientSocket: Socket) {}
+
+  async onCall(clientSocket: Socket) {}
+
+  async onCheck(clientSocket: Socket) {}
+
+  async onFold(clientSocket: Socket) {}
+
+  async onRaise(clientSocket: Socket) {}
+
+  errorEmitter(clientSocket: Socket, errorMessage: string) {
+    clientSocket.emit("ERROR", errorMessage);
+  }
+
+  messageEmitter(clientSocket: Socket, message: string) {
+    clientSocket.emit("MESSAGE", message);
+  }
 }
